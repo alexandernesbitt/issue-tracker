6276a5ac8e85f92674403870b7b02084f4ab3b62
Matteo Cominetti <matteo@cominetti.org>
2015-10-30 12:31:05 -0300

    start

---
+++ ARUP.IssueTracker2/Classes/RestCallback.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+using  System.Windows;
+
+namespace ARUP.IssueTracker.Classes
+{
+    public static class RestCallback
+    {
+        public static bool Check(IRestResponse response)
+        {
+            try
+            {
+                if (null == response)
+                {
+                    string error = "Please check your connection.";
+                    MessageBox.Show(error, "Unknown error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+
+
+
+                if (response.StatusCode != System.Net.HttpStatusCode.OK
+                    && response.StatusCode != System.Net.HttpStatusCode.Created
+                    && response.StatusCode != System.Net.HttpStatusCode.NoContent
+                    && response != null)
+                {
+                    object ve;
+                    if (RestSharp.SimpleJson.TryDeserializeObject(response.Content, out  ve))
+                    {
+                        ErrorMsg validationErrorResponse = RestSharp.SimpleJson.DeserializeObject<ErrorMsg>(response.Content);
+                        string error = "";
+                        if (validationErrorResponse.errorMessages.Any())
+                        {
+                            foreach (var str in validationErrorResponse.errorMessages)
+                            {
+                                error += str + "\n";
+                            }
+
+                        }
+                        error += (null != validationErrorResponse.errors && validationErrorResponse.errors.ToString().Replace(" ", "") != "{}") ? validationErrorResponse.errors.ToString() : "";
+                        MessageBox.Show(error, response.StatusDescription, MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    else
+                    {
+                        string error = response.StatusDescription;
+                        if (string.IsNullOrWhiteSpace(error))
+                            error = "Please check your connection.";
+                        MessageBox.Show(error, response.ResponseStatus.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    return false;
+                }
+                
+            }
+            catch (Exception ex1)
+            {
+                MessageBox.Show("exception: " + ex1);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ARUP.IssueTracker2/UserControls/JiraPanel.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using ARUP.IssueTracker.Classes;
+
+namespace ARUP.IssueTracker.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MainPanel.xaml
+    /// </summary>
+    public partial class JiraPanel : UserControl
+    {
+        public event EventHandler<IntArg> ComponentsShowBCFEH;
+
+        public JiraPanel()
+        {
+            InitializeComponent();
+
+        }
+        public int projIndex
+        {
+            get { return projCombo.SelectedIndex; }
+            set { projCombo.SelectedIndex = value; }
+        }
+       
+
+        public int listIndex
+        {
+            get { return issueList.SelectedIndex; }
+            set { issueList.SelectedIndex = value; }
+        }
+        private void ComponentsShow(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                if (issueList.SelectedIndex != -1)
+                {
+                    if (ComponentsShowBCFEH != null)
+                    {
+                        ComponentsShowBCFEH(this, new IntArg(issueList.SelectedIndex));
+                    }
+                }
+            }
+            catch (System.Exception ex1)
+            {
+                MessageBox.Show("exception: " + ex1);
+            }
+        }
+
+        public string Filters
+        {
+            get
+            {
+                string filters = "";
+                if (customFilters.IsEnabled)
+                {
+                    filters = statusfilter.Result + typefilter.Result + priorityfilter.Result;
+                }
+                return filters;
+            }
+
+        }
+        public string Order
+        {
+            get
+            {
+                string ordertype = "DESC";
+                string orderdate = "Updated";
+                foreach (RadioButton rb in grouptype.Children)
+                {
+                    if (rb.IsChecked.Value)
+                        ordertype = rb.Content.ToString();
+
+                }
+                foreach (RadioButton rb in groupdate.Children)
+                {
+                    if (rb.IsChecked.Value)
+                    {
+                        orderdate = rb.Content.ToString();
+                    }
+
+                }
+
+                return "+ORDER+BY+" + orderdate + "+" + ordertype;
+            }
+
+        }
+        public void clearFilters_Click(object sender, RoutedEventArgs e)
+        {
+            statusfilter.Clear();
+            typefilter.Clear();
+            priorityfilter.Clear();
+           
+                //IM.getIssues();
+        }
+        private void ChangeStatus_Click(object sender, RoutedEventArgs e)
+        {
+            //   IM.ChangeStatus_Click();
+        }
+        private void ChangePriority_Click(object sender, RoutedEventArgs e)
+        {
+            //  IM.ChangePriority_Click();
+        }
+       
+        
+        private void ChangeType_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChangeAssign_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        
+    }
+       
+}
