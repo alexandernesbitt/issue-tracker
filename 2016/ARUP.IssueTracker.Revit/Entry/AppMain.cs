e3d98aee86f84d28c936325f30eb642c69354beb
Yamin Tengono <yamin.tengono@arup.com>
2015-11-06 12:00:49 +1100

    Update Revit main app and command to support 2016

--- 2016/ARUP.IssueTracker.Revit/Entry/AppMain.cs
+++ 2016/ARUP.IssueTracker.Revit/Entry/AppMain.cs
@@ -10,154 +10,154 @@ using Autodesk.Revit.UI;
 namespace ARUP.IssueTracker.Revit.Entry
 {
 
-  /// <summary>
-  /// Obfuscation Ignore for External Interface
-  /// </summary>
-  [Obfuscation(Exclude = true, ApplyToMembers = false)]
-  [Transaction(TransactionMode.Manual)]
-  public class AppMain : IExternalApplication
-  {
-    private string _path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+	/// <summary>
+	/// Obfuscation Ignore for External Interface
+	/// </summary>
+	[Obfuscation(Exclude = true, ApplyToMembers = false)]
+	[Transaction(TransactionMode.Manual)]
+	public class AppMain : IExternalApplication
+	{
+		private string _path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-    #region Revit IExternalApplciation Implementation
+		#region Revit IExternalApplciation Implementation
 
-    /// <summary>
-    /// Startup
-    /// </summary>
-    /// <param name="application"></param>
-    /// <returns></returns>
-    public Result OnStartup(UIControlledApplication application)
-    {
+		/// <summary>
+		/// Startup
+		/// </summary>
+		/// <param name="application"></param>
+		/// <returns></returns>
+		public Result OnStartup(UIControlledApplication application)
+		{
 
-      try
-      {
+			try
+			{
 
-        // Version
-          if (!application.ControlledApplication.VersionName.Contains("2014") && !application.ControlledApplication.VersionName.Contains("2015"))
-          {
-              using (TaskDialog td = new TaskDialog("Cannot Continue"))
-              {
-                  td.TitleAutoPrefix = false;
-                  td.MainInstruction = "Incompatible Revit Version";
-                  td.MainContent = "This Add-In was built for Revit 2014 and 2015, please contact CASE for assistance.";
-                  td.Show();
-              }
-              return Result.Cancelled;
-          }
+				// Version
+				if (!application.ControlledApplication.VersionName.Contains("2014") && !application.ControlledApplication.VersionName.Contains("2015") && !application.ControlledApplication.VersionName.Contains("2016"))
+				{
+					using (TaskDialog td = new TaskDialog("Cannot Continue"))
+					{
+						td.TitleAutoPrefix = false;
+						td.MainInstruction = "Incompatible Revit Version";
+						td.MainContent = "This Add-In was built for Revit 2014, 2015 and 2016, please contact CASE for assistance.";
+						td.Show();
+					}
+					return Result.Cancelled;
+				}
 
-        // Master Tab 
-        const string c_tabName = "CASE";
+				// Master Tab 
+				const string c_tabName = "CASE";
 
-        try
-        {
-          // Create the Tab
-          application.CreateRibbonTab(c_tabName);
-        }
-        catch { }
+				try
+				{
+					// Create the Tab
+					application.CreateRibbonTab(c_tabName);
+				}
+				catch { }
 
-        // Assembly Paths
-        string m_issuetracker = Path.Combine(ProgramFilesx86(), "CASE", "ARUP Issue Tracker", "ARUP.IssueTracker.dll");
+				// Assembly Paths
+				string m_issuetracker = Path.Combine(ProgramFilesx86(), "CASE", "ARUP Issue Tracker", "ARUP.IssueTracker.dll");
 
-        // Check that File Exists
-        if (!File.Exists(m_issuetracker))
-        {
-          using (TaskDialog td = new TaskDialog("Cannot Continue"))
-          {
-            td.TitleAutoPrefix = false;
-            td.MainInstruction = "Required Issue Tracker Library Not Found";
-            td.MainContent = m_issuetracker;
-            td.Show();
-          }
-          return Result.Cancelled;
-        }
-        
-        // Load Assemblies
-        Assembly.LoadFrom(m_issuetracker);
+				// Check that File Exists
+				if (!File.Exists(m_issuetracker))
+				{
+					using (TaskDialog td = new TaskDialog("Cannot Continue"))
+					{
+						td.TitleAutoPrefix = false;
+						td.MainInstruction = "Required Issue Tracker Library Not Found";
+						td.MainContent = m_issuetracker;
+						td.Show();
+					}
+					return Result.Cancelled;
+				}
 
-        // Tab
-        RibbonPanel m_panel = application.CreateRibbonPanel(c_tabName, "Arup Issue Tracker");
+				// Load Assemblies
+				Assembly.LoadFrom(m_issuetracker);
 
-        // Button Data
-        PushButton m_pushButton = m_panel.AddItem(new PushButtonData("Issue Tracker",
-                                                                     "Issue Tracker", 
-                                                                     Path.Combine(_path, "ARUP.IssueTracker.Revit.dll"),
-                                                                     "ARUP.IssueTracker.Revit.Entry.CmdMain")) as PushButton;
+				// Tab
+				RibbonPanel m_panel = application.CreateRibbonPanel(c_tabName, "Arup Issue Tracker");
 
-        // Images and Tooltip
-        if (m_pushButton != null)
-        {
-          m_pushButton.Image = LoadPngImgSource("ARUP.IssueTracker.Assets.ARUPIssueTrackerIcon16x16.png", m_issuetracker);
-          m_pushButton.LargeImage = LoadPngImgSource("ARUP.IssueTracker.Assets.ARUPIssueTrackerIcon32x32.png", m_issuetracker);
-          m_pushButton.ToolTip = "Arup Issue Manager";
-        }
-      }
-      catch (Exception ex1)
-      {
-        MessageBox.Show("exception: " + ex1);
-        return Result.Failed;
-      }
+				// Button Data
+				PushButton m_pushButton = m_panel.AddItem(new PushButtonData("Issue Tracker",
+																								 "Issue Tracker",
+																								 Path.Combine(_path, "ARUP.IssueTracker.Revit.dll"),
+																								 "ARUP.IssueTracker.Revit.Entry.CmdMain")) as PushButton;
 
-      return Result.Succeeded;
-    }
+				// Images and Tooltip
+				if (m_pushButton != null)
+				{
+					m_pushButton.Image = LoadPngImgSource("ARUP.IssueTracker.Assets.ARUPIssueTrackerIcon16x16.png", m_issuetracker);
+					m_pushButton.LargeImage = LoadPngImgSource("ARUP.IssueTracker.Assets.ARUPIssueTrackerIcon32x32.png", m_issuetracker);
+					m_pushButton.ToolTip = "Arup Issue Manager";
+				}
+			}
+			catch (Exception ex1)
+			{
+				MessageBox.Show("exception: " + ex1);
+				return Result.Failed;
+			}
 
-    /// <summary>
-    /// Shut Down
-    /// </summary>
-    /// <param name="application"></param>
-    /// <returns></returns>
-    public Result OnShutdown(UIControlledApplication application)
-    {
-      return Result.Succeeded;
-    }
+			return Result.Succeeded;
+		}
 
-    #endregion
+		/// <summary>
+		/// Shut Down
+		/// </summary>
+		/// <param name="application"></param>
+		/// <returns></returns>
+		public Result OnShutdown(UIControlledApplication application)
+		{
+			return Result.Succeeded;
+		}
 
-    #region Private Members
+		#endregion
 
-    /// <summary>
-    /// Get System Architecture
-    /// </summary>
-    /// <returns></returns>
-    static string ProgramFilesx86()
-    {
-        if (8 == IntPtr.Size || (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"))))
-            return Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+		#region Private Members
 
-        return Environment.GetEnvironmentVariable("ProgramFiles");
-    }
+		/// <summary>
+		/// Get System Architecture
+		/// </summary>
+		/// <returns></returns>
+		static string ProgramFilesx86()
+		{
+			if (8 == IntPtr.Size || (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"))))
+				return Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+
+			return Environment.GetEnvironmentVariable("ProgramFiles");
+		}
 
 
-    /// <summary>
-    /// Load an Image Source from File
-    /// </summary>
-    /// <param name="sourceName"></param>
-    /// <param name="path"></param>
-    /// <returns></returns>
-    private ImageSource LoadPngImgSource(string sourceName, string path)
-    {
+		/// <summary>
+		/// Load an Image Source from File
+		/// </summary>
+		/// <param name="sourceName"></param>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		private ImageSource LoadPngImgSource(string sourceName, string path)
+		{
 
-      try
-      {
-        // Assembly & Stream
-        Assembly m_assembly = Assembly.LoadFrom(Path.Combine(path));
-        Stream m_icon = m_assembly.GetManifestResourceStream(sourceName);
+			try
+			{
+				// Assembly & Stream
+				Assembly m_assembly = Assembly.LoadFrom(Path.Combine(path));
+				Stream m_icon = m_assembly.GetManifestResourceStream(sourceName);
 
-        // Decoder
-        PngBitmapDecoder m_decoder = new PngBitmapDecoder(m_icon, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+				// Decoder
+				PngBitmapDecoder m_decoder = new PngBitmapDecoder(m_icon, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
 
-        // Source
-        ImageSource m_source = m_decoder.Frames[0];
-        return (m_source);
+				// Source
+				ImageSource m_source = m_decoder.Frames[0];
+				return (m_source);
 
-      }
-      catch { }
+			}
+			catch { }
 
-      // Fail
-      return null;
+			// Fail
+			return null;
 
-    }
+		}
 
-    #endregion
+		#endregion
 
-  }
+	}
 }
