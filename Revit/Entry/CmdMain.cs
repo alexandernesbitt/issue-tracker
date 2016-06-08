d9f17a8662f0effa3748b00717b7c65a928c402a
unknown <huan-ting.chen@SINLT5CG5390XFD.clients.global.arup.com>
2016-06-08 17:53:26 +0800

    re-organized project folder structure to share the same source code files among different versions

---
+++ Revit/Entry/CmdMain.cs
@@ -0,0 +1,88 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Reflection;
+
+namespace ARUP.IssueTracker.Revit.Entry
+{
+
+	/// <summary>
+	/// Obfuscation Ignore for External Interface
+	/// </summary>
+	[Obfuscation(Exclude = true, ApplyToMembers = false)]
+	[Transaction(TransactionMode.Manual)]
+	[Regeneration(RegenerationOption.Manual)]
+	public class CmdMain : IExternalCommand
+	{
+		internal static CmdMain ThisCmd = null;
+		private static bool _isRunning;
+		private static AppIssueTracker _appIssueTracker;
+
+		/// <summary>
+		/// Main Command Entry Point
+		/// </summary>
+		/// <param name="commandData"></param>
+		/// <param name="message"></param>
+		/// <param name="elements"></param>
+		/// <returns></returns>
+		public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+		{
+			try
+            {
+#if REVIT2014
+                string versionNumber = "2014";
+#elif REVIT2015
+                string versionNumber = "2014";
+#else
+                string versionNumber = "2016";
+#endif
+
+                // Version
+                if (!commandData.Application.Application.VersionName.Contains(versionNumber))
+				{
+					using (TaskDialog td = new TaskDialog("Cannot Continue"))
+					{
+						td.TitleAutoPrefix = false;
+						td.MainInstruction = "Incompatible Revit Version";
+                        td.MainContent = string.Format("This Add-In was built for Revit {0}, please find the Arup Issue Tracker group in Yammer for assistance...", versionNumber);
+						td.Show();
+					}
+					return Result.Cancelled;
+				}
+
+				// Form Running?
+				if (_isRunning && _appIssueTracker != null && _appIssueTracker.RvtWindow.IsLoaded)
+				{
+					_appIssueTracker.Focus();
+					return Result.Succeeded;
+				}
+
+				_isRunning = true;
+
+				ThisCmd = this;
+				_appIssueTracker = new AppIssueTracker();
+				_appIssueTracker.ShowForm(commandData.Application);
+
+                // register a document closed event
+                commandData.Application.Application.DocumentClosed += Application_DocumentClosed;
+
+				return Result.Succeeded;
+
+			}
+			catch (Exception e)
+			{
+				message = e.Message;
+				return Result.Failed;
+			}
+
+		}
+
+        void Application_DocumentClosed(object sender, Autodesk.Revit.DB.Events.DocumentClosedEventArgs e)
+        {
+            _appIssueTracker.CloseForm();
+        }
+
+	}
+
+}
