702797f9098ab8716de3e271aa6fd64bdfc4d6dd
Huan-ting Chen <Huan-ting.Chen@arup.com>
2017-02-27 15:28:27 +0800

    release build: 2017.02.27.01

--- ARUP.IssueTracker.Win/Properties/AssemblyInfo.cs
+++ ARUP.IssueTracker.Win/Properties/AssemblyInfo.cs
@@ -49,6 +49,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.02.20.01")]
-[assembly: AssemblyFileVersion("2017.02.20.01")]
+[assembly: AssemblyVersion("2017.02.27.01")]
+[assembly: AssemblyFileVersion("2017.02.27.01")]
 [assembly: GuidAttribute("9FC6BA85-A348-40B4-97C4-6699DA912C21")]
